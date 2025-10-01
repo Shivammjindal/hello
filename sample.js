f171a8ccfe9550581d015ea73ec916da0379be6f
Shivam Jindal <shivamjindals2002@gmail.com>
2025-10-01 10:08:50 +0530

    A

--- sample.js
+++ sample.js
@@ -5,7 +5,7 @@ import express from "express"
 
 const app = express()
 
-app.use(express.json())
+app.use(expre. ss.json())
 app.use(express.urlencoded({extended:true}))
 
 console.log('hello world')
